a13ed198dd41a4301bdbc406f1bcdb090a90534f
RENISHKURANI <renishkurani5@gmail.com>
2025-06-21 14:24:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Tool Renting Website - Home Page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
